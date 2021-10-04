8caf7ca1d0da55bc126ad06bfb2f2310dbb5f052
Josh Jacob <josh.jacob@rogers.com>
2021-10-04 17:56:14 -0400

    Contact form added.

---
+++ frontend/components/Contactpage/ContactForm.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function ContactForm() {
+    return (
+        <div className='flex w-full bg-gray-100 py-16'>
+            <div className='container'>
+                <div className='text-center w-1/2 m-auto'>
+                    <h3 className='font-jcHeading2 font-medium text-base mt-4 mb-8 text-jcBlue'>Contact Us</h3>
+                    <h2 className='font-jcHeading2 font-bold text-3xl w-4/5 m-auto'>How can we help you?</h2>
+                </div>
+                
+                <form>
+                    
+                    
+                    
+                </form>
+                
+            </div>
+        </div>
+    )
+}
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,11 +1,13 @@
 import Layout from '@/components/Layout';
 import BaseShowcase from '@/components/BaseShowcase'
 import HomeContact from '@/components/Homepage/HomeContact'
+import ContactForm from '@/components/Contactpage/ContactForm'
 
 export default function ContactPage() {
     return (
         <Layout>
             <BaseShowcase pageTitle="Contact Us" tagLine="We create brand new corporate identities!" />
+            <ContactForm />
             <HomeContact />
         </Layout>
     );
